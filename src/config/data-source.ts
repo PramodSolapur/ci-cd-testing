64346d0e484eee94aba698f4323e1603f729a892
PramodSolapur <psolapur5@gmail.com>
2025-08-14 16:43:16 +0530

    changed synchronize false

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
     username: Config.DB_USERNAME,
     password: Config.DB_PASSWORD,
     database: Config.DB_NAME,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [Todo],
     migrations: ['src/migration/*.ts'],
